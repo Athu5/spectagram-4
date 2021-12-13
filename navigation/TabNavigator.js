3c013ebd73c86876a7d12dc0051d97663cad1a3c
Athu5 <84505685+Athu5@users.noreply.github.com>
2021-12-13 23:54:38 +0530

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Feed from '../screens/Feed';
+import CreatePost from '../screens/CreatePost';
+
+const Tab = createMaterialBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+    return(
+        <Tab.Navigator
+        labeled={false}
+        barStyle={StyleSheet.bottomTabStyles}
+            screenOptions = {({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if(route.name === 'Feed' ) {
+                        iconName = focused
+                        ? 'book'
+                        : 'book-outline';
+                    }else if(route.name === 'CreatePost') {
+                        iconName = focused ? 'create' : 'create-outline';
+                    }
+
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+            tabBarOptions={{
+                ActiveTintColor: '#ee8249',
+                inactiveTintColor: 'gray',
+            }}
+        >
+            <Tab.Screen name="Feed" component={Feed}  />
+            <Tab.Screen name="CreatePost" component={CreatePost}/>
+        </Tab.Navigator>
+    );
+}
+
+export default BottomTabNavigator
---
+++ temp_posts.json
@@ -0,0 +1,12 @@
+[
+    {
+        "caption":"post1",
+        "author":"by xyz1",
+        "description":"xyz1"
+    },
+    {
+        "caption":"post2",
+        "author":"by xyz2",
+        "description":"xyz2"
+    }
+]
